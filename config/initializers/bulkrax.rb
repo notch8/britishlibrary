24e79a490e2472fe207de7ad06189c5ca9a80a72
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2026-02-09 22:08:23 +0000

    set record_split_count to 1 million for csv parser

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -239,3 +239,10 @@ if ENV.fetch('HYKU_BULKRAX_ENABLED', false)
   end
   # rubocop:enable Metrics/BlockLength
 end
+
+# Override bulkrax CSV parse to allow big ol files (for use with metadata only exports)
+Bulkrax::CsvParser.clas_eval do
+    def records_split_count
+      1_000_000
+    end
+end
